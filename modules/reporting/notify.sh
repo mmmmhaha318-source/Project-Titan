60b16c6d1a9df1bf1d865bd6a5df27801b3780f2
mmmmhaha318-source <mmmmhaha318@gmail.com>
2025-11-25 01:11:38 +0300

    Delete modules directory

--- modules/reporting/notify.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TITLE=$1
-MESSAGE=$2
-
-python3 core/discord_notifier.py "$TITLE" "$MESSAGE"
--- modules/vuln_scan/web_basic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET=$1
-RECON_DIR="results/$TARGET/recon"
-VULN_DIR="results/$TARGET/vuln_scan"
-mkdir -p $VULN_DIR
-echo "[TITAN] Basic vulnerability scan initiated for $TARGET..."
-nuclei -l $RECON_DIR/live_hosts.txt -t "technologies,cves,misconfiguration,vulnerabilities" -o $VULN_DIR/nuclei_findings.txt
-echo "[TITAN] Basic vulnerability scan completed for $TARGET."
